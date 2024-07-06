fd4ba1653ed228dc1696aebbc827644d7d20aa2c
first-order-coder <manethginura@gmail.com>
2024-07-06 22:42:09 +0300

    Day_14

---
+++ Sams_TeachUrSelf/changing_images/main.js
@@ -0,0 +1,11 @@
+let previewElement = document.querySelector('.preview-image')
+
+function firstImage () {
+    previewElement.src = 'images/1.jpg'
+}
+function secondImage () {
+    previewElement.src = 'images/2.jpg'
+}
+function thirdImage () {
+    previewElement.src = 'images/3.jpg'
+}
