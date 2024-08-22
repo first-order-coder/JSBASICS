3f81c06d6497e2aedfe6b627f947dbdcc105d01c
first-order-coder <manethginura@gmail.com>
2024-08-22 22:08:18 +0300

    Day_22

--- Sams_TeachUrSelf/event_handling/define_event.js
+++ Sams_TeachUrSelf/event_handling/define_event.js
@@ -10,9 +10,20 @@ butnElm.onclick = mousealert;
 // this way the html part will be completely free of js code
 function myFunc(event){
     console.log(event.key); //event key is used on onkeydown event to get the pressed key
+    console.log(event.which); //event.which will return the ascii version of the pressed key.
     let value = event.button; //event.button is used to get the mouse button that was clicked
     console.log(value)
 }
 butnElm.onkeydown = myFunc; // and event.button is only available for mouse events like mousedown or mouseup --> thats why undefined is returned in results.
 butnElm.onmousedown = myFunc;
+
+let divElm = document.querySelector('#div-1');
+divElm.tabindex = 0; // Make the div focusable 
+
+function divFunc(event){
+    let keyPressed = event.key;
+    divElm.innerHTML += keyPressed
+}
+divElm.onkeydown = divFunc; //the div element is not really focusable so we need to write something first before so we can focus on first
+// divElm.onkeydown = divFunc;
 
