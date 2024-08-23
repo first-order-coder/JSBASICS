ee3bdbf576f790b528c32efe431055af070cef80
first-order-coder <manethginura@gmail.com>
2024-08-23 21:46:22 +0300

    Day_23

--- Sams_TeachUrSelf/change_div/main.js
+++ Sams_TeachUrSelf/change_div/main.js
@@ -13,3 +13,17 @@ function specsShowing(){
     };
 }
 showSpecs.onmousedown = specsShowing;
+
+function colorChange(){
+    showSpecs.style.color = "red";
+    showSpecs.style.fontWeight = "bold";
+    showSpecs.style.textDecoration = "underline";
+}
+showSpecs.onmouseover = colorChange;
+
+function colorUnchange(){
+    showSpecs.style.color = "rgb(188, 156, 156)";
+    showSpecs.style.fontWeight = "none";
+    showSpecs.style.textDecoration = "none";
+}
+showSpecs.onmouseleave = colorUnchange;
