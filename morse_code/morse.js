e6a1526343f68c5f453096992d070af4bf6903e6
first-order-coder <manethginura@gmail.com>
2024-10-23 19:17:40 +0300

    Day_25

---
+++ morse_code/morse.js
@@ -0,0 +1,32 @@
+let inputElm = document.querySelector('#input-elm')
+let submitBtnElm = document.querySelector('#submit-btn')
+
+const morseCode = { 'A': '.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+
+let submitInput = () => {
+    let inputVal = inputElm.value;
+    console.log(morseCode.Y)
+    console.log(inputVal)
+    // conditionLogic();
+}
+
+submitBtnElm.onclick = submitInput;
+
+let conditionLogic = () => {
+}
+
+
